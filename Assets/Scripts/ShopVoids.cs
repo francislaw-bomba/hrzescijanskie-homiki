520d082b68475d65277065576a230c7c5bf5bdb7
francislaw-bomba <119966985+francislaw-bomba@users.noreply.github.com>
2024-04-29 22:55:38 +0200

    56yr6yrty

--- Assets/Scripts/ShopVoids.cs
+++ Assets/Scripts/ShopVoids.cs
@@ -12,37 +12,58 @@ public class ShopVoids : MonoBehaviour
 
     public void BuyLemonSeeds()
     {
-        PlayerVal.Money -= 300;
-        PlayerVal.Lemon.SeedCount += 1;
+        if (PlayerVal.Money >= 300)
+        {
+            PlayerVal.Money -= 300;
+            PlayerVal.Lemon.SeedCount += 1;
+        }
     }
     public void BuyMangoSeeds()
     {
-        PlayerVal.Money -= 500;
-        PlayerVal.Mango.SeedCount += 1;
+        if (PlayerVal.Money >= 500)
+        {
+            PlayerVal.Money -= 500;
+            PlayerVal.Mango.SeedCount += 1;
+        }
     }
     public void BuyDragonFruitSeeds()
     {
-        PlayerVal.Money -= 100;
-        PlayerVal.DragonFruit.SeedCount += 1;
+        if (PlayerVal.Money >= 100)
+        {
+            PlayerVal.Money -= 100;
+            PlayerVal.DragonFruit.SeedCount += 1;
+        }
     }
     public void BuyPineappleSeeds()
     {
-        PlayerVal.Money -= 100;
-        PlayerVal.Pineapple.SeedCount += 1;
+        if (PlayerVal.Money >= 100)
+        {
+            PlayerVal.Money -= 100;
+            PlayerVal.Pineapple.SeedCount += 1;
+        }
     }
     public void BuyPurpleLemonSeeds()
     {
-        PlayerVal.Money -= 100;
-        PlayerVal.PurpleLemon.SeedCount += 1;
+        if(PlayerVal.Money >= 1000)
+        {
+            PlayerVal.Money -= 1000;
+            PlayerVal.PurpleLemon.SeedCount += 1;
+        }
     }
-    public void BuyBlueTomatoSeeds()
+    public void BuyBlueTomatoSeeds()   
     {
-        PlayerVal.Money -= 100;
-        PlayerVal.BlueTomato.SeedCount += 1;
+        if (PlayerVal.Money >= 100)
+        {
+            PlayerVal.Money -= 100;
+            PlayerVal.BlueTomato.SeedCount += 1;
+        }
     }
     public void BuyTurqPeachSeeds()
     {
-        PlayerVal.Money -= 100;
-        PlayerVal.TurqPeach.SeedCount += 1;
+        if (PlayerVal.Money >= 100)
+        {
+            PlayerVal.Money -= 100;
+            PlayerVal.TurqPeach.SeedCount += 1;
+        }
     }
 }
